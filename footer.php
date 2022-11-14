3d3187865261715bf2ed10112ded742b912af62d
Suez Sohan <suez.sohan@gmail.com>
2022-11-14 18:22:47 +0600

    Change footer contents

--- footer.php
+++ footer.php
@@ -1,25 +1,25 @@
 <div class="footCollum">
 
     <div class="colLeft" style="width:33.33%; height:100%; border-right:1px solid white; padding: 0px 15px;">
-        <h1 style="color:#f15c3c;">Apply for job</h1>
+        <h1 style="color:#f15c3c;">Biggan Cafe</h1>
         <p style="margin-top: 9px; color: #838383;">© 2022 SuezSohan.</p>
     </div>
 
     <div class="colMiddle" style="width:33.33%; height:100%; border-right:1px solid white; padding: 0px 15px;">
-        <p style="color:white; font-size: 19px;">Library</p>
+        <p style="color:white; font-size: 19px;">About Us</p>
         <ul style="margin-top: 5px;line-height: 16px; list-style:none">
-            <li><a href="#">Literature collection of us</a></li>
-            <li><a href="#">Photography collection of us</a></li>
-            <li><a href="#">News paper monthly published by students</a></li>
+            <li><a href="#">Our Activities</a></li>
+            <li><a href="#">Our Achievements</a></li>
+            <li><a href="#">Why are we different from others?</a></li>
         </ul>
     </div>
 
     <div class="colRight" style="width:33.33%; height:100%; padding: 0px 15px;">
-        <p style="color:white; font-size: 19px;">About us</p>
+        <p style="color:white; font-size: 19px;">Developed By</p>
         <ul style="margin-top: 5px;line-height: 16px; list-style:none;" >
-            <li><a href="#">Visit our team leader</a></li>
-            <li><a href="#">Our activities and other works</a></li>
-            <li><a href="#">Goal of which makes us different</a></li>
+            <li>Suez Sohan</li>
+            <li>Department of</li>
+            <li> Mechanical Engineering (CUET)|<a href="#"> See more</a></li>
         </ul>
     </div>
 
