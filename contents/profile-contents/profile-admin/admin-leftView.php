72c13dfe317db7534cf58b5981fde7d91ebad13a
Suez Sohan <suez.sohan@gmail.com>
2022-11-14 20:29:50 +0600

    Solve spelling mistake of admin left view

--- contents/profile-contents/profile-admin/admin-leftView.php
+++ contents/profile-contents/profile-admin/admin-leftView.php
@@ -8,7 +8,7 @@
             <ul>
                 <li><a href="index.php?request=profile&profileMain&requestProfileMain=setExam"><i class="fa fa-chevron-circle-right"></i> Set Exam Date [Admin only]</a></li>
                 <li><a href="index.php?request=profile&profileMain&requestProfileMain=usrPrivilage"><i class="fa fa-chevron-circle-right"></i> Set User Privilage [Admin only]</a></li>
-                <li><a href="index.php?request=profile&profileMain&requestProfileMain=inputMarks"><i class="fa fa-chevron-circle-right"></i> Input Exam Marks [Admin adn Teacher]</a></li>
+                <li><a href="index.php?request=profile&profileMain&requestProfileMain=inputMarks"><i class="fa fa-chevron-circle-right"></i> Input Exam Marks [Admin and Teacher]</a></li>
                 <li><a href="index.php?request=profile&profileMain&requestProfileMain=stdInfoView"><i class="fa fa-chevron-circle-right"></i> See Student Information [Admin and Student(Indevidual)]</a></li>
                 <li><a href="index.php?request=profile&profileMain&requestProfileMain=adminExamResult"><i class="fa fa-chevron-circle-right"></i> Exam Result [Admin, Teacher and Student(Indevidual)]</a></li>
             </ul>
